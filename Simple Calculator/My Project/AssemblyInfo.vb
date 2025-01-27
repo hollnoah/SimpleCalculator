554a14a69df47ce892a7465a8563ead2fa9a937b
Noah Holloway <noahholloway23@gmail.com>
2025-01-26 19:00:41 -0700

    Add project files.

---
+++ Simple Calculator/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Simple Calculator")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Razer")>
+<Assembly: AssemblyProduct("Simple Calculator")>
+<Assembly: AssemblyCopyright("Copyright © Razer 2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("1c561e0d-4a72-4946-b419-1ba62b70bf69")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
